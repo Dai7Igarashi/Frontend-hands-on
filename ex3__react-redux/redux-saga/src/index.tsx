e29f0a2db9f7a2edc5efcba6f43855162b23c78b
Dai7Igarashi <40178733+Dai7Igarashi@users.noreply.github.com>
2019-01-31 22:46:11 +0900

    Pre Finish redux-saga async

--- ex3__react-redux/redux-saga/src/index.tsx
+++ ex3__react-redux/redux-saga/src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import App from './containers/App';
+import rootSaga from './sagas';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = configureStore();
+store.runSaga(rootSaga);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
